672d7fa66409bdec8a4921d56ceb55ff3dcb40d7
Zuhal <77770731+zuhalcode@users.noreply.github.com>
2024-08-17 12:28:58 +0700

    fixing axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Axios Instance setup
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
