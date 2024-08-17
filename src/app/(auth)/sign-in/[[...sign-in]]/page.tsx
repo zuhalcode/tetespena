29875e4477a1f2329294b60344da04ff43b1abec
Zuhal <77770731+zuhalcode@users.noreply.github.com>
2024-08-17 12:39:05 +0700

    fixing clerk

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,6 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <SignIn
-      fallbackRedirectUrl={"/dashboard"}
       appearance={{
         elements: {
           formButtonPrimary: "bg-blue-500",
